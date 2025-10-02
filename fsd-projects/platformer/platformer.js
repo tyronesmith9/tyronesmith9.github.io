f7528e6b1a1e8750b23eaa61ab9612f2e46397fb
tyronesmith9 <tyrone.smith@carvercollegiate.org>
2025-10-02 14:37:58 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -14,7 +14,7 @@ $(function () {
       firstTimeSetup = false;
       //start game
       setInterval(main, 1000 / frameRate);
-    }
+    
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
@@ -27,28 +27,44 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(100,700,150,15,"black");
+    createPlatform(200,600,150,15,"blue");
+    createPlatform(100,500,150,15,"black");
+    createPlatform(200,600,150,15,"black"); 
+    createPlatform(300,600,150,15,"black");
+    createPlatform(100,500,150,15,"black")
+    createPlatform(300,400,150,15,"black")
+    createPlatform(500,300,150,15,"black")
+    createPlatform(800,200,150,15,"black")
+    createPlatform(800,100,120,15,"yellow") 
+    createPlatform(900,400,120,15,"black")
+    createPlatform(1000,300,150,15,"black")
+    createPlatform(950,500,150,15,"black")
+    createPlatform(1100,600,150,15,"black")
+    createPlatform(1100,400,190,15,"black")
+    createPlatform(1100,500,150,15,"black")
     // TODO 3 - Create Collectables
-
+    createCollectable("diamond",800,30,0.5,0.5)
+    createCollectable("diamond",500,30,0.5,0.5)
 
 
     
     // TODO 4 - Create Cannons
-
-
+    createCannon("right",600,1500);
+    createCannon("right",700,1800);
+    createCannon("right",300,1000)
+    createCannon("bottom",800,100)
     
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
   }
-
+  }
   registerSetup(setup);
+
 });
